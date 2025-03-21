ec2c362884e0c4bc6d50014a36c6b81abe28828a
AlexandrLAE <125304406+AlexandrLAE@users.noreply.github.com>
2025-03-21 19:04:02 +0300

    TEST

---
+++ airflow/dags/dag_test.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+
+
+default_args = {
+    "owner": "market",
+    "depends_on_past": False,
+    "start_date": datetime(2025, 3, 21),
+    #"retry_delay": timedelta(minutes=0.1)
+}
+
+dag = DAG('dag_test_1', default_args=default_args, schedule_interval='0 1 * * *', catchup=True,
+          max_active_tasks=2, max_active_runs=1, tags=["Test", "My first dag"])
+
+task1 = BashOperator(
+    task_id='task1',
+    bash_command='python3 /airflow/scripts/dag_test_1/task1.py',
+    dag=dag)
+
+task2 = BashOperator(
+    task_id='task2',
+    bash_command='python3 /airflow/scripts/dag_test_1//task2.py',
+    dag=dag)
+
+task1 >> task2
