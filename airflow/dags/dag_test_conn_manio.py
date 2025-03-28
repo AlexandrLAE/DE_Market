15030c4ab145ff95f74d30f6ed69825aee3b5fd8
AlexandrLAE <125304406+AlexandrLAE@users.noreply.github.com>
2025-03-28 18:25:12 +0300

    19

--- airflow/dags/dag_test_conn_manio.py
+++ airflow/dags/dag_test_conn_manio.py
@@ -8,8 +8,20 @@ def test_minio_connection():
     buckets = hook.list_buckets()  # Получаем список бакетов
     print(f"Доступные бакеты в MinIO: {buckets}")
 
-with DAG('test_minio_connection', start_date=datetime(2025, 3, 28)) as dag:
-    test_task = PythonOperator(
-        task_id='test_minio',
+default_args = {
+    "owner": "market",
+    "depends_on_past": False,
+    "start_date": datetime(2025, 3, 28),
+    #"retry_delay": timedelta(minutes=0.1)
+}
+
+dag = DAG('test_minio', default_args=default_args, schedule_interval='0 1 * * *', catchup=True,
+          max_active_tasks=2, max_active_runs=1, tags=["test_minio_connection"])
+
+task1 = PythonOperator (
+    task_id='test_minio',
         python_callable=test_minio_connection
-    )
+        dag=dag)
+
+task1 
+
