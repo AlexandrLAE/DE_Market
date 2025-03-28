638660766d04d568a1edd789c26f45f88ae25a15
AlexandrLAE <125304406+AlexandrLAE@users.noreply.github.com>
2025-03-28 21:33:24 +0300

    21

--- airflow/dags/dag_test_conn_manio.py
+++ airflow/dags/dag_test_conn_manio.py
@@ -3,10 +3,35 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
+import boto3
+from airflow.hooks.base import BaseHook
+
 def test_minio_connection():
-    hook = S3Hook(aws_conn_id='minio_conn')
-    buckets = hook.list_buckets()  # Получаем список бакетов
+    conn = BaseHook.get_connection('Minio')
+
+    s3 = boto3.resource('s3',
+                        endpoint_url=conn.host,
+                        aws_access_key_id=conn.login,
+                        aws_secret_access_key=conn.password
+    )
+    s3client = s3.meta.client 
+
+    #and then you can use boto3 methods for manipulating buckets and files
+    #for example:
+
+    bucket = s3.Bucket('ozon')
+    # Iterates through all the objects, doing the pagination for you. Each obj
+    # is an ObjectSummary, so it doesn't contain the body. You'll need to call
+    # get to get the whole body.
+    for obj in bucket.objects.all():
+        key = obj.key
+    buckets = s3client.list_buckets()  # Получаем список бакетов
     print(f"Доступные бакеты в MinIO: {buckets}")
+
+# def test_minio_connection():
+#     hook = S3Hook(aws_conn_id='minio_conn')
+#     buckets = hook.list_buckets()  # Получаем список бакетов
+#     print(f"Доступные бакеты в MinIO: {buckets}")
 
 default_args = {
     "owner": "market",
