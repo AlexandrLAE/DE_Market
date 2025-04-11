a355a7da8746fcbddaa7b0d07ebb87a58f839522
AlexandrLAE <125304406+AlexandrLAE@users.noreply.github.com>
2025-04-11 21:33:25 +0300

    certbot

---
+++ certbot/generate-certificate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# чистим папку, где могут находиться старые сертификаты
+rm -rf /etc/letsencrypt/live/certfolder*
+
+# выдаем себе сертификат (обратите внимание на переменные среды)
+certbot certonly --standalone --email $DOMAIN_EMAIL -d $DOMAIN_URL --cert-name=certfolder --key-type rsa --agree-tos
+
